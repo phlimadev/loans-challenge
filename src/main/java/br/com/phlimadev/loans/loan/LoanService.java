3f31350459ff8478720f474347ce7f04067f3ecf
phlimadev <devphlima@gmail.com>
2025-05-28 14:21:03 -0300

    feat: creating a method that evaluates salary range and age/location

--- src/main/java/br/com/phlimadev/loans/loan/LoanService.java
+++ src/main/java/br/com/phlimadev/loans/loan/LoanService.java
@@ -19,12 +19,25 @@ public class LoanService {
         }
     }
 
+    public boolean assessWhetherTheSalarIsBetween3000And5000(BigDecimal income) {
+        return income.compareTo(BigDecimal.valueOf(3000)) > 0 && income.compareTo(BigDecimal.valueOf(5000)) <= 0;
+    }
+
+    public void assessAgeAndLocation(Integer age, String location) {
+        if (age < 30 && location.equalsIgnoreCase("SP")) {
+            loans.add(LoanEnum.PERSONAL);
+            loans.add(LoanEnum.GUARANTEED);
+        }
+    }
+
     public CustomerLoansDTO evaluateLoanRequest(CustomerDTO data) {
         Integer age = data.age();
         BigDecimal income = data.income();
         String location = data.location();
 
         assessWhetherTheSalaryIsEqualToOrLessThan3000(income);
-
+        if (assessWhetherTheSalarIsBetween3000And5000(income)) {
+            assessAgeAndLocation(age, location);
+        }
     }
 }
