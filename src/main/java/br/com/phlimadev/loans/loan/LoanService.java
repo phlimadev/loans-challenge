5edb75bb8f69ca630e5021e42d41f5190f2d0967
phlimadev <devphlima@gmail.com>
2025-05-28 15:57:46 -0300

    refactor: encapsulating helper methods

--- src/main/java/br/com/phlimadev/loans/loan/LoanService.java
+++ src/main/java/br/com/phlimadev/loans/loan/LoanService.java
@@ -11,25 +11,25 @@ import java.util.Set;
 @Service
 public class LoanService {
 
-    public boolean assessWhetherTheSalaryIsEqualToOrLessThan3000(BigDecimal income) {
+    private boolean assessWhetherTheSalaryIsEqualToOrLessThan3000(BigDecimal income) {
         return income.compareTo(BigDecimal.valueOf(3000)) <= 0;
     }
 
-    public boolean assessWhetherTheSalaryIsBetween3000And5000(BigDecimal income) {
+    private boolean assessWhetherTheSalaryIsBetween3000And5000(BigDecimal income) {
         return income.compareTo(BigDecimal.valueOf(3000)) > 0 && income.compareTo(BigDecimal.valueOf(5000)) <= 0;
     }
 
-    public boolean assessAgeAndLocation(Integer age, String location) {
+    private boolean assessAgeAndLocation(Integer age, String location) {
         return age < 30 && location.equalsIgnoreCase("SP");
     }
 
-    public void assessIfTheSalaryIsEqualOrGreaterThan5000(BigDecimal income, Set<LoanDTO> loans) {
+    private void assessIfTheSalaryIsEqualOrGreaterThan5000(BigDecimal income, Set<LoanDTO> loans) {
         if (income.compareTo(BigDecimal.valueOf(5000)) >= 0) {
             loans.add(new LoanDTO(LoanEnum.CONSIGNMENT.getType(), LoanEnum.CONSIGNMENT.getInterestRate()));
         }
     }
 
-    public void addPersonalAndGuaranteedLoans(Set<LoanDTO> loans) {
+    private void addPersonalAndGuaranteedLoans(Set<LoanDTO> loans) {
         loans.add(new LoanDTO(LoanEnum.PERSONAL.getType(), LoanEnum.PERSONAL.getInterestRate()));
         loans.add(new LoanDTO(LoanEnum.GUARANTEED.getType(), LoanEnum.GUARANTEED.getInterestRate()));
     }
