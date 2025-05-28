fe9b5b94787eb1362970eec7592759bda82fa838
phlimadev <devphlima@gmail.com>
2025-05-28 15:04:00 -0300

    perf: performing code: streamlining classes with boolean returns and extracting code repetition

--- src/main/java/br/com/phlimadev/loans/loan/LoanService.java
+++ src/main/java/br/com/phlimadev/loans/loan/LoanService.java
@@ -11,28 +11,27 @@ import java.util.Set;
 @Service
 public class LoanService {
 
-    public void assessWhetherTheSalaryIsEqualToOrLessThan3000(BigDecimal income, Set<LoanDTO> loans) {
-        if (income.compareTo(BigDecimal.valueOf(3000)) <= 0) {
-            loans.add(new LoanDTO(LoanEnum.PERSONAL.getType(), LoanEnum.PERSONAL.getInterestRate()));
-            loans.add(new LoanDTO(LoanEnum.GUARANTEED.getType(), LoanEnum.GUARANTEED.getInterestRate()));
-        }
+    public boolean assessWhetherTheSalaryIsEqualToOrLessThan3000(BigDecimal income) {
+        return income.compareTo(BigDecimal.valueOf(3000)) <= 0;
     }
 
     public boolean assessWhetherTheSalaryIsBetween3000And5000(BigDecimal income) {
         return income.compareTo(BigDecimal.valueOf(3000)) > 0 && income.compareTo(BigDecimal.valueOf(5000)) <= 0;
     }
 
-    public void assessAgeAndLocation(Integer age, String location, Set<LoanDTO> loans) {
-        if (age < 30 && location.equalsIgnoreCase("SP")) {
-            loans.add(new LoanDTO(LoanEnum.PERSONAL.getType(), LoanEnum.PERSONAL.getInterestRate()));
-            loans.add(new LoanDTO(LoanEnum.GUARANTEED.getType(), LoanEnum.GUARANTEED.getInterestRate()));
-        }
+    public boolean assessAgeAndLocation(Integer age, String location) {
+        return age < 30 && location.equalsIgnoreCase("SP");
     }
 
     public void assessIfTheSalaryIsEqualOrGreaterThan5000(BigDecimal income, Set<LoanDTO> loans) {
         if (income.compareTo(BigDecimal.valueOf(5000)) >= 0) {
             loans.add(new LoanDTO(LoanEnum.CONSIGNMENT.getType(), LoanEnum.CONSIGNMENT.getInterestRate()));
         }
+    }
+
+    public void addPersonalAndGuaranteedLoans(Set<LoanDTO> loans) {
+        loans.add(new LoanDTO(LoanEnum.PERSONAL.getType(), LoanEnum.PERSONAL.getInterestRate()));
+        loans.add(new LoanDTO(LoanEnum.GUARANTEED.getType(), LoanEnum.GUARANTEED.getInterestRate()));
     }
 
     public CustomerLoansDTO evaluateLoanRequest(CustomerDTO data) {
@@ -42,12 +41,15 @@ public class LoanService {
         BigDecimal income = data.income();
         String location = data.location();
 
-        assessWhetherTheSalaryIsEqualToOrLessThan3000(income, loans);
-        if (assessWhetherTheSalaryIsBetween3000And5000(income)) {
-            assessAgeAndLocation(age, location, loans);
+        if (assessWhetherTheSalaryIsEqualToOrLessThan3000(income)) {
+            addPersonalAndGuaranteedLoans(loans);
+        }
+        if (assessWhetherTheSalaryIsBetween3000And5000(income) && assessAgeAndLocation(age, location)) {
+            addPersonalAndGuaranteedLoans(loans);
         }
 
         assessIfTheSalaryIsEqualOrGreaterThan5000(income, loans);
+
         return new CustomerLoansDTO(data.name(), loans);
     }
 }
