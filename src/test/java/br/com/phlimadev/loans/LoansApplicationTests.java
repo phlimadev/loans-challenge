abdb4735f6a217f59f243393f572a9b03bad2771
phlimadev <devphlima@gmail.com>
2025-05-28 13:49:36 -0300

    first commit

---
+++ src/test/java/br/com/phlimadev/loans/LoansApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.phlimadev.loans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
